673a0f197bc44f8ff8c24a05f7df3aeb8fd193c3
maearnhart <earnhart.m1@gmail.com>
2023-11-27 10:18:34 -0900

    Started to add class definitions.

--- codeBase/ToDoClass.h
+++ codeBase/ToDoClass.h
@@ -9,10 +9,30 @@ class ToDo {
 
     public:
 
+    string Task;
+
+    int month, day, year;
+
     // publicly avalible varibles
 
 
     // publicly avalible functions
+
+    Task(){
+        Task = "Assaignment";
+        month = 1;
+        day = 1;
+        year = 2000;
+    }
+    Task(string Task, int month, int day, int year){
+        Task = Task;
+        month = month;
+        day = day;
+        year = year;
+    }
+    void Print(){
+        
+    }
 
     private:
 
